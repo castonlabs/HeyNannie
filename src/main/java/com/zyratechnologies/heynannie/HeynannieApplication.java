290007d879d8729384bcd708e37049558a509d28
Caston mubvumbi <castonmubvumbi@gmail.com>
2026-06-26 19:25:44 +0200

    added the class the main class the entry point of the application

---
+++ src/main/java/com/zyratechnologies/heynannie/HeynannieApplication.java
@@ -0,0 +1,13 @@
+package com.zyratechnologies.heynannie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeynannieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeynannieApplication.class, args);
+    }
+
+}
