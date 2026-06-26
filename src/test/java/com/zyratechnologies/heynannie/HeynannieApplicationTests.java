a28222072053956a7206d1f46d8978a8254a1d8a
Caston mubvumbi <castonmubvumbi@gmail.com>
2026-06-26 19:26:32 +0200

    created A Test class

---
+++ src/test/java/com/zyratechnologies/heynannie/HeynannieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyratechnologies.heynannie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeynannieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
