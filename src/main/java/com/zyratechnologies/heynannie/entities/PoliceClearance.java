d8b1a966183f7bcf26dc69a0472d6f6b06d3b640
Caston mubvumbi <castonmubvumbi@gmail.com>
2026-07-02 05:04:46 +0200

    Created an Entity class PoliceClearance

---
+++ src/main/java/com/zyratechnologies/heynannie/entities/PoliceClearance.java
@@ -0,0 +1,54 @@
+package com.zyratechnologies.heynannie.entities;
+
+import com.zyratechnologies.heynannie.enums.DocumentStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "police_clearances")
+public class PoliceClearance {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+    @Column(nullable = false)
+
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "nanny_id", nullable = false, unique = true)
+    private Nanny nanny;
+
+    @NotNull(message = " document id must not be null")
+    private int documentId;
+
+    @Column(nullable = false)
+    @NotNull(message = " document id must not be null")
+    private String imageUrl;
+
+    @Column(nullable = false)
+
+
+    @Enumerated(EnumType.STRING)
+    private DocumentStatus documentStatus;
+
+   @Column(nullable = false)
+    private String rejectionReason;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void prePersist() {
+        documentStatus = DocumentStatus.PENDING;
+    }
+
+
+
+}
