9463407f2d923b02cc0aed0a4f724ec9ede212e1
Caston mubvumbi <castonmubvumbi@gmail.com>
2026-07-02 06:21:38 +0200

    Created enum for documentType

---
+++ src/main/java/com/zyratechnologies/heynannie/enums/DocumentType.java
@@ -0,0 +1,11 @@
+package com.zyratechnologies.heynannie.enums;
+
+public enum DocumentType {
+    NATIONAL_ID,
+    POLICE_CLEARANCE,
+    PSYCHIATRIC_TEST,
+    OLEVEL_RESULTS,
+    DRIVERS_LICENCE,
+    EMPLOYMENT_HISTORY,
+    REFERENCE
+}
