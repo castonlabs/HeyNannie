351bd98518d57350f84ef181b3a4267d139059a6
Caston mubvumbi <castonmubvumbi@gmail.com>
2026-07-02 10:34:15 +0200

    made some changes removed the password and email

--- src/main/java/com/zyratechnologies/heynannie/entities/Parent.java
+++ src/main/java/com/zyratechnologies/heynannie/entities/Parent.java
@@ -26,11 +26,6 @@ public class Parent {
     @Column(nullable = false)
     private String lastName;
 
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
 
     @Column(nullable = false)
     private String phoneNumber;
@@ -46,6 +41,10 @@ public class Parent {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private NannieStatus status;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
