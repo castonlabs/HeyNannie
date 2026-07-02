4d5718d3b7575f9634df7108cdfc45a9eaa1e616
Caston mubvumbi <castonmubvumbi@gmail.com>
2026-07-02 06:46:38 +0200

    Created Admin Entity

---
+++ src/main/java/com/zyratechnologies/heynannie/entities/AdminUser.java
@@ -0,0 +1,60 @@
+package com.zyratechnologies.heynannie.entity;
+
+import com.zyratechnologies.heynannie.enums.AdminRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "admin_users")
+public class AdminUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AdminRole role;
+
+    private Boolean isActive;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        isActive = true;
+        role = AdminRole.JUNIOR_ADMIN;
+    }
+}
